5d8d1b40aef520d42aa1bd976d4ea3238fe8d97d
Carlos Alvarado <tec.carlos.a@gmail.com>
2024-09-27 19:53:05 -0500

    Add carts api and cats vieww component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
+// // import { useState } from 'react'
+// import './App.css'
+// // import { ApiCoffeCarousel } from './components/ApiCoffeCarousel' 
+// import { CatGalleryFetch } from './components/CatGalleryFetch' 
+
+// function App() {
+//   // const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <CatGalleryFetch/>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default App
 import './App.css'
-import { ApiCoffeCarousel } from './components/ApiCoffeCarousel' 
+//import { ApiCoffeeCarruselBasic } from './components/ApiCoffeeCarruselBasic'
+import { CatGalleryFetch } from './components/CatGalleryFetch';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <ApiCoffeCarousel/>
-      </div>
-    </>
+    <div>
+      <CatGalleryFetch />
+    </div>
   )
 }
 
