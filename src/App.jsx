5f11f82f867c78cd4daf20569156a80c02cfa42d
Carlos Alvarado <tec.carlos.a@gmail.com>
2024-09-27 21:07:56 -0500

    Add group activity, api rick and morty

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-// // import { useState } from 'react'
-// import './App.css'
-// // import { ApiCoffeCarousel } from './components/ApiCoffeCarousel' 
-// import { CatGalleryFetch } from './components/CatGalleryFetch' 
-
-// function App() {
-//   // const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <CatGalleryFetch/>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
 import './App.css'
+import { RickAndMorty } from './components/RickAndMorty'
 //import { ApiCoffeeCarruselBasic } from './components/ApiCoffeeCarruselBasic'
-import { CatGalleryFetch } from './components/CatGalleryFetch';
+// import { CatGalleryFetch } from './components/CatGalleryFetch';
 
 function App() {
 
 
   return (
     <div>
-      <CatGalleryFetch />
+      <RickAndMorty />
     </div>
   )
 }
